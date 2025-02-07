0a6cc9ba9e326a2ef53dc73d641d5b822b82c071
m3122 <3122mackenzie@gmail.com>
2025-02-07 19:07:29 -0300

    added links to the albums

--- randomalbum/src/app/page.tsx
+++ randomalbum/src/app/page.tsx
@@ -21,6 +21,23 @@ export default function Home() {
       <img src={album.cover} alt={`${album.album} cover`} className="w-72 h-72 object-cover mb-5" />
       <p className="text-xl font-semibold">{album.album}</p>
       <p className="text-lg text-gray-600">{album.artist}</p>
+      <div className="flex justify-center space-x-2 mt-3">
+        {album.spotify && (
+          <a href={album.spotify} target="_blank" rel="noopener noreferrer">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/8/84/Spotify_icon.svg" alt="Spotify" className="w-6 h-6" />
+          </a>
+        )}
+        {album.bandcamp && (
+          <a href={album.bandcamp} target="_blank" rel="noopener noreferrer">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/d/d0/Bandcamp-button-circle-aqua.svg" alt="Bandcamp" className="w-6 h-6" />
+          </a>
+        )}
+        {album.soundcloud && (
+          <a href={album.soundcloud} target="_blank" rel="noopener noreferrer">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/a/a2/Antu_soundcloud.svg" alt="SoundCloud" className="w-6 h-6" />
+          </a>
+        )}
+      </div>
       <button onClick={handleRefresh} className="mt-5 px-5 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
         Refresh Album
       </button>
