819c83d2afbbf1483f2a696efd00491eeac6dedb
m3122 <3122mackenzie@gmail.com>
2025-02-10 17:01:06 -0300

    ignored eslint

--- randomalbum/next.config.ts
+++ randomalbum/next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true, // Ignore ESLint rules during builds
+  },
+  /* other config options here */
 };
 
 export default nextConfig;
